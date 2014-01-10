5706deb844a6075cc63ef2c506c938e86c816c1a
Laspando <pandocr7@gmail.com>
2014-01-10 09:42:13 +0700

    file silsilah.php(silsilah suku batak)

---
+++ silsilah.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>batak</title>
+<link rel="stylesheet" type="text/css" href="css.css" />
+</head>
+
+<body>
+
+   
+   <div  id="wrapper">
+   
+         
+         <div id="header">
+         
+		 <center><h1>BUKU BATAK</h1>
+         <h3>BUDAYAKU BATAK TOBA</h3>
+         <h1><font face="Toba">Budayaku Batak Toba</h1></font>
+          
+         <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="sejarah.PHP">SEJARAH</a></li>
+        <li><a href="galerry.php">GALLERY</a></li>
+        <li><a href="video.php">VIDEO</a></li>
+        <li><a href="">SILSILAH</a></li>
+        <li><a href="aksara.php">AKSARA BATAK</a></li>
+        
+        </ul>
+		       		 
+			  </center>	 
+		 </div>
+		
+		 <div  id="faux">
+		
+		       
+			   
+<h2 align="center">SILSILAH DAN SEJARAH MARGA BATAK DARI RAJA BATAK</h2>
+<ul>
+<p>Pembagian utama Si RAJA BATAK  :</p>
+<li>1.	Guru Tateabulan</li>
+<li>2.	Raja Isumbaon</li>
+</ul>
+<ul>
+<p>Belahan yang dinamakan LOTUNG, yang mencakup kelompok suku yang sebenernya, yaitu Himpunan BORBOR, dan juga sejumlah marga yang lebih kecil, berasal dari Guru Tateabulan.
+Yang dinamakan belahan SUMBA yang ke dalamnya termasuk sisa kelompok suku dan marga lainnya, berasal dari Raja Isumbaon.</p>
+<p>Yang termasuk BELAHAN LOTUNG ada 5 yaitu :</p>
+<li>1.	Raja biakbiak</li>
+<li>2.	Saribu Raja</li>
+<p>Mempunyai 3 Kelompok yaitu</p>
+1.	LONTUNG
+2.	BORBOR
+3.	BABIAT
+3.	Limbong Mulana
+<p>Mempunyai 1 Kelompok yaitu Limbong (Habeahan)</p>
+4.	Sagala Raja
+<p>Mempunyai 1 kelompok yaitu Sagala</p>
+5.	Malau Raja
+<p>Mempunyai 4 kelompok yaitu</p>
+1.	Paseraja – Malau
+2.	Manik
+3.	Ambarita
+4.	Gurning
+<p>Rupanya, raja Biakbiak pergi ke Aceh. Tidak diketahui, apakah ia meninggalkan keturunan.
+Limbong pada pokoknya mendiami suatu lembah di sebelah selatan penggung gunung, yang menghubungkan Pusuk Buhit dengan tanah datar, dan Sagala Raja Lembah yang ke arah utara punggung gunung.</p>
+<p>Malau Raja tersebar di kawasan sekeliling Pangururan (pulau dan tanah diseberangnya), dan dengan memakai nama Damanik, ia adalah marga yang memerintah di wilayah swapraja Siantar di Sumatera Timur.</p>
+<p>Belahan SUMBA meliputi :</p>
+1.	Tuan Sori – mangaraja
+<p>mempunyai 3 kelompok :</p>
+1.	Nai Ambaton
+2.	Nai Rasaon (R.Mangarerak)
+3.	Nai Suanon(Tuan Sorbadibanua)
+2.	Raja ni Asiasi
+<p>Pertama saya akan membahas tentang pembagian Belahan LONTUNG :</p>
+1.	LONTUNG
+<p>Yang mempunyai anak meliputi :</p>
+1.	Situmorang
+<p>Mempunyai anak :</p>
+1.	Lumban Pande
+2.	Lumban Nahor
+3.	Suhut ni Huta
+4.	Siringoringo
+<p>Mempunyai anak :</p>
+1.	Lumban Toruan
+2.	Sipangpang
+3.	Rumapea
+5.	Sitohang uruk
+6.	Sitohang tonga – tonga
+7.	Sitohang toruan (Lumban Gaol)
+
+
+
+         </ div>	   
+        
+         <div id="footer">
+		       
+              <center>Create By: laspando@gmail.com and te@m GO Green</center>		
+
+         </div>
+		 
+		 
+   </ div>
+  
+</body>
+</html>
