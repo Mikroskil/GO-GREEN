fa7655b277162e90e68525cd342045b4d7a0cd8f
Laspando <pandocr7@gmail.com>
2014-01-10 09:46:13 +0700

    file aksara.php (aksara batak)

---
+++ aksara.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>batak</title>
+<link rel="stylesheet" type="text/css" href="css.css" />
+</head>
+
+<body>
+
+   
+   <div id="wrapper">
+   
+         
+         <div id="header">
+         
+		 <center><h1>BUKU BATAK</h1>
+         <h3>BUDAYAKU BATAK TOBA</h3>
+         <h1><font face="Toba">Budayaku Batak Toba</h1></font>
+          
+         <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="sejarah.php">SEJARAH</a></li>
+        <li><a href="galerry.php">GALLERY</a></li>
+        <li><a href="video.php">VIDEO</a></li>
+        <li><a href="silsilah.php">SILSILAH</a></li>
+        <li><a href=" ">AKSARA BATAK</a></li>
+        
+        </ul>
+		       		 
+			  </center>	 
+		 </div>
+		 
+		 <div id="faux">
+		
+		      
+			   
+			  
+			   <h3>Surat Batak adalah nama aksara yang digunakan untuk menuliskan bahasa Batak. Surat Batak masih berkerabat dengan aksara Nusantara lainnya. Aksara ini memiliki beberapa varian bentuk, tergantung bahasa dan wilayah. Secara garis besar, ada lima varian surat Batak di Sumatra, yaitu Karo, Toba, Dairi, Simalungun, dan Mandailing. Aksara ini wajib diketahui oleh para datu, yaitu orang yang dihormati oleh masyarakat Batak karena menguasai ilmu sihir, ramal, dan penanggalan. Kini, aksara ini masih dapat ditemui dalam berbagai pustaha, yaitu kitab tradisional masyarakat Batak.</h3>
+                       <table height="255" align="center" cellspacing="5">
+               <tr>
+                   <td><img src="aksara 1.jpg" style="border:4px solid #000" alt="Aksara Batak" width="290" height="200"  /></td>
+                   <td><img src="aksara 2.jpg" style="border:4px solid #000" alt="Aksara Batak" width="290" height="200"  /></td> 
+                   <td><img src="aksara 3.jpg" style="border:4px solid #000" alt="Aksara Batak" width="295" height="200"  /></td>              
+               </tr>
+               <tr>
+                   <td><img src="aksara 4.png" style="border:4px solid #000" alt="Aksara Batak" width="290" height="200"  /></td>
+                   <td><img src="aksara 5.jpg" style="border:4px solid #000" alt="Aksara Batak" width="290" height="200"  /></td> 
+                   <td><img src="1.jpg" style="border:4px solid #000" alt="Aksara Batak" width="295" height="200"  /></td>              
+               </tr>
+</table>
+               
+</table><br />
+                <br /><br />
+					
+		       
+         </div>	   
+        
+         <div id="footer">
+		       
+              <center>Create By: laspando@gmail.com and te@m GO Green</center>		
+
+         </div>
+		 
+		 
+   </div>
+   
+</body>
+</html>
