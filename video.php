0e4618b32761cceb6dae9777e6503b4d13513487
Laspando <pandocr7@gmail.com>
2014-01-17 10:23:19 +0700

    File video.php (vidoe tor-tor batak)

---
+++ video.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>batak</title>
+<link rel="stylesheet" type="text/css" href="css.css" />
+</head>
+
+<body>
+
+  
+   <div id="wrapper">
+   
+         
+         <div id="header">
+         
+		 <center><h1>BUKU BATAK</h1>
+         <h3>BUDAYAKU BATAK TOBA</h3>
+         <h1><font face="Toba">Budayaku Batak Toba</h1></font>
+          
+         <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="sejarah.PHP">SEJARAH</a></li>
+        <li><a href="galerry.php">GALLERY</a></li>
+        <li><a href=" ">VIDEO</a></li>
+        <li><a href="silsilah.php">SILSILAH</a></li>
+        <li><a href="aksara.php">AKSARA BATAK</a></li>
+        
+        </ul>
+		       		 
+			  </center>	 
+		 </div>
+		
+		 <div id="faux">
+		
+ <table align="center"  width="500" height="300" border="1"cellspacing="30" cellpadding="1">
+ <tr>
+ <td>            
+ <table align="center"  width="300" height="300" border="0">
+ <tr>
+   <td>
+    <h3 align="center">Tortor Somba</h3>
+   </td>
+ </tr>
+ <tr>
+   <td>
+   <video width="400" height="440" controls>
+   <source src="Tortor Somba.mp4" type="video/mp4">
+ </ video>
+</td>  
+</tr>
+
+</table>
+</td>
+<td>
+<table align="center"  width="300" height="300" border="0">
+ <tr>
+   <td>
+    <h3 align="center">Tortor Sipitu Sawan</h3>
+   </td>
+ </tr>
+ 
+ <tr>
+   <td align="justify" valign="bottom">
+   <video width="400" height="440" controls>
+   <source src="sipitu sawan.mp4" type="video/mp4">
+ </ video>
+</td> 
+</tr>
+
+</table>
+</td>
+</tr>
+ <table align="center"  width="500" height="300" border="1" cellspacing="30" cellpadding="1">
+ <tr>
+ <td>            
+ <table align="center"  width="300" height="300" border="0">
+ <tr>
+   <td>
+    <h3 align="center">Tortor Penyambutan Pengantin dan Pasahaton Ulos</h3>
+   </td>
+ </tr>
+ <tr>
+   <td>
+   <video width="400" height="440" controls>
+   <source src="Tortor Pengantin.mp4" type="video/mp4">
+ </ video>
+</td> 
+</tr>
+
+</table>
+</td>
+<td>
+<table align="center"  width="300" height="300" border="0">
+ <tr>
+   <td>
+    <h3 align="center">Tortor Tunggal Panaluan</h3>
+   </td>
+ </tr>
+ 
+ <tr>
+   <td>
+   <video width="400" height="440" controls>
+   <source src="5tortor tunggal.mp4" type="video/mp4">
+ </ video>
+</td> 
+</tr>
+
+</table>
+</td>
+</tr>
+</table>
+</div>	   
+         
+         <div id="footer">
+		       
+              <center>Create By: laspando@gmail.com and te@m GO Green</center>		
+
+         </div>
+		 
+		 
+   </div>
+   
+</body>
+</html>
