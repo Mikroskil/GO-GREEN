d32b1f602b87c76b5b7f30bf73ee93ce7efb81b6
Laspando <pandocr7@gmail.com>
2014-01-17 11:25:20 +0700

    Perubahan code program pada file silsilah.php

--- silsilah.php
+++ silsilah.php
@@ -8,10 +8,10 @@
 
 <body>
 
+   <!-- Begin Wrapper -->
+   <div id="wrapper">
    
-   <div  id="wrapper">
-   
-         
+         <!-- Begin Header -->
          <div id="header">
          
 		 <center><h1>BUKU BATAK</h1>
@@ -30,76 +30,82 @@
 		       		 
 			  </center>	 
 		 </div>
+		 <!-- End Header -->
+		 
+         <!-- Begin Faux Columns -->
+		 <div id="faux">
 		
-		 <div  id="faux">
-		
-		       
+		       <!-- Begin Left Column -->
+		     
+		       <!-- End Right Column -->
 			   
 <h2 align="center">SILSILAH DAN SEJARAH MARGA BATAK DARI RAJA BATAK</h2>
 <ul>
 <p>Pembagian utama Si RAJA BATAK  :</p>
-<li>1.	Guru Tateabulan</li>
-<li>2.	Raja Isumbaon</li>
-</ul>
-<ul>
+<b>1.	Guru Tateabulan</b><br />
+<b>2.	Raja Isumbaon</b><br />
+
+
 <p>Belahan yang dinamakan LOTUNG, yang mencakup kelompok suku yang sebenernya, yaitu Himpunan BORBOR, dan juga sejumlah marga yang lebih kecil, berasal dari Guru Tateabulan.
 Yang dinamakan belahan SUMBA yang ke dalamnya termasuk sisa kelompok suku dan marga lainnya, berasal dari Raja Isumbaon.</p>
-<p>Yang termasuk BELAHAN LOTUNG ada 5 yaitu :</p>
-<li>1.	Raja biakbiak</li>
-<li>2.	Saribu Raja</li>
-<p>Mempunyai 3 Kelompok yaitu</p>
-1.	LONTUNG
-2.	BORBOR
-3.	BABIAT
-3.	Limbong Mulana
+<h4><b>Yang termasuk BELAHAN LOTUNG ada 5 yaitu :</h4>
+ 1.	Raja biakbiak<br />
+ 2.	Saribu Raja</b><br />
+<p><b>Mempunyai 3 Kelompok yaitu</b></p>
+1.	LONTUNG<br />
+2.	BORBOR<br />
+3.	BABIAT<br />
+<b>3.	Limbong Mulana<br />
 <p>Mempunyai 1 Kelompok yaitu Limbong (Habeahan)</p>
 4.	Sagala Raja
 <p>Mempunyai 1 kelompok yaitu Sagala</p>
 5.	Malau Raja
-<p>Mempunyai 4 kelompok yaitu</p>
-1.	Paseraja – Malau
-2.	Manik
-3.	Ambarita
-4.	Gurning
+<p>Mempunyai 4 kelompok yaitu</p></b>
+1.	Paseraja – Malau<br />
+2.	Manik<br />
+3.	Ambarita<br />
+4.	Gurning<br />
 <p>Rupanya, raja Biakbiak pergi ke Aceh. Tidak diketahui, apakah ia meninggalkan keturunan.
 Limbong pada pokoknya mendiami suatu lembah di sebelah selatan penggung gunung, yang menghubungkan Pusuk Buhit dengan tanah datar, dan Sagala Raja Lembah yang ke arah utara punggung gunung.</p>
 <p>Malau Raja tersebar di kawasan sekeliling Pangururan (pulau dan tanah diseberangnya), dan dengan memakai nama Damanik, ia adalah marga yang memerintah di wilayah swapraja Siantar di Sumatera Timur.</p>
-<p>Belahan SUMBA meliputi :</p>
+<p><b>Belahan SUMBA meliputi :</p>
 1.	Tuan Sori – mangaraja
-<p>mempunyai 3 kelompok :</p>
-1.	Nai Ambaton
-2.	Nai Rasaon (R.Mangarerak)
-3.	Nai Suanon(Tuan Sorbadibanua)
-2.	Raja ni Asiasi
-<p>Pertama saya akan membahas tentang pembagian Belahan LONTUNG :</p>
-1.	LONTUNG
-<p>Yang mempunyai anak meliputi :</p>
+<p>mempunyai 3 kelompok :</b></p>
+1	Nai Ambaton<br />
+2.	Nai Rasaon (R.Mangarerak)<br />
+3.	Nai Suanon(Tuan Sorbadibanua)<br />
+2.	Raja ni Asiasi<br />
+<p<b>Pertama saya akan membahas tentang pembagian Belahan LONTUNG :</p>
+1.LONTUNG
+<p><b>Yang mempunyai anak meliputi :</b></p>
 1.	Situmorang
-<p>Mempunyai anak :</p>
-1.	Lumban Pande
-2.	Lumban Nahor
-3.	Suhut ni Huta
-4.	Siringoringo
-<p>Mempunyai anak :</p>
-1.	Lumban Toruan
-2.	Sipangpang
-3.	Rumapea
-5.	Sitohang uruk
-6.	Sitohang tonga – tonga
+<p><b>Mempunyai anak :</b></p>
+1.	Lumban Pande<br />
+2.	Lumban Nahor<br />
+3.	Suhut ni Huta<br />
+4.	Siringoringo<br />
+<p><b>Mempunyai anak :</b></p>
+1.	Lumban Toruan<br />
+2.	Sipangpang<br />
+3.	Rumapea<br />
+5.	Sitohang uruk<br />
+6.	Sitohang tonga – tonga<br />
 7.	Sitohang toruan (Lumban Gaol)
 
 
 
-         </ div>	   
-        
+         </div>	   
+         <!-- End Faux Columns --> 
+
+         <!-- Begin Footer -->
          <div id="footer">
 		       
               <center>Create By: laspando@gmail.com and te@m GO Green</center>		
 
          </div>
+		 <!-- End Footer -->
 		 
-		 
-   </ div>
-  
+   </div>
+   <!-- End Wrapper -->
 </body>
 </html>
